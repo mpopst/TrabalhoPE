02574046ad0eb7bd97a7aff046389285bccb1147
mpopst <ma.popst@hotmail.com>
2019-11-05 00:48:26 -0300

    Exercicio 1 pronto

--- exercicio1.py
+++ exercicio1.py
@@ -1,11 +1,21 @@
 from util import validar_matriz
 import numpy as np
 
-def exercicio1(P: np.array, i: int, j: int, epsilon: float):
+def exercicio1(P: np.array, i: int, j: int, n: int, qtd: int):
     validar_matriz(P,5)
-    p_minus = np.array([1 if elem == i else 0 for elem in range(1,6)])
-    #p = p_minus.__copy__()
-    p = np.array([0 for elem in range(1,6)])
-    while (np.linalg.norm(p - p_minus, ord=np.inf) < epsilon): #TODO ver como que faz isso em Python
-        p_minus, p = p, np.matmul(p_minus,P)
-    return p[j-1]
+    return sum(1 for k in range(qtd) if simulacao(P, i, n) == j)/qtd
+
+
+def simulacao(P: np.array, i: int, n: int):
+    X = i-1
+    somas_parciais = np.cumsum(P, 1)
+    size = P.shape[0]
+    for k in range(0,n):
+        aleatorio = np.random.rand()
+        s = 0
+        l = -1
+        while s<aleatorio and l<size:
+            l+=1
+            s+=somas_parciais[X,l]
+        X = l
+    return X+1
--- test_all.py
+++ test_all.py
@@ -1,5 +1,5 @@
 import unittest
-from exercicio1 import exercicio1
+from exercicio1 import exercicio1, simulacao
 from exercicio3 import _executa_xn_, martingal
 from util import validar_matriz
 import numpy as np
@@ -52,9 +52,37 @@ class TestExercicio3(unittest.TestCase):
     
     def test_exercicio1_dadosexercico1(self):
         P = np.matrix([[1/3, 0, 2/3, 0, 0],[1/4,1/2,1/4,0,0],[1/2, 0, 1/2, 0, 0], [0, 0, 0, 0, 1],[0,0,0,2/3,1/3]])
-        i, j, epsilon = 1,1,0.1
-        exercicio1(P, i, j, epsilon)
+        i, j, n, qtd = 1,1,5,20
+        actualResult = exercicio1(P, i, j, n, qtd)
         self.assertTrue(True)
+        
+        
+    def test_exercicio1_simulacaoDadosExercicio1(self):
+        P = np.matrix([[1/3, 0, 2/3, 0, 0],[1/4,1/2,1/4,0,0],[1/2, 0, 1/2, 0, 0], [0, 0, 0, 0, 1],[0,0,0,2/3,1/3]])
+        i, n = 4, 7
+        result_list = [4,5]
+        actual_result = simulacao(P, i, n)
+        self.assertTrue(actual_result in result_list)
+        
+    def test_exercicio1_simulacaoDadosExercicio2(self):
+        P = np.matrix([[1/3, 0, 2/3, 0, 0],[1/4,1/2,1/4,0,0],[1/2, 0, 1/2, 0, 0], [0, 0, 0, 0, 1],[0,0,0,2/3,1/3]])
+        i, n = 3, 7
+        result_list = [1,2,3]
+        actual_result = simulacao(P, i, n)
+        self.assertTrue(actual_result in result_list)
+        
+    def test_exercicio1_exercico1(self):
+        P = np.matrix([[1/3, 0, 2/3, 0, 0],[1/4,1/2,1/4,0,0],[1/2, 0, 1/2, 0, 0], [0, 0, 0, 0, 1],[0,0,0,2/3,1/3]])
+        i, j, n, qtd = 2, 4, 20, 20
+        expected_result = 0
+        actual_result = exercicio1(P, i, j, n, qtd)
+        self.assertEqual(actual_result, expected_result)
+        
+    def test_exercicio1_exercico1_Teste2(self):
+        P = np.matrix([[1/3, 0, 2/3, 0, 0],[1/4,1/2,1/4,0,0],[1/2, 0, 1/2, 0, 0], [0, 0, 0, 0, 1],[0,0,0,2/3,1/3]])
+        i, j, n, qtd = 4, 4, 20, 20
+        actual_result = exercicio1(P, i, j, n, qtd)
+        self.assertTrue(actual_result > 0)
         
         
 if __name__ == '__main__':
