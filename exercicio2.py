88d268cf71113c9836a630372ee694c5494c5926
mpopst <ma.popst@hotmail.com>
2019-11-05 01:27:11 -0300

    Feito exercicio 2

--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,9 @@
 import numpy as np
-from util import validar_matriz
+from util import validar_matriz, simulaca_markov
 
-def exercicio2(P: np.matrix, X_0: int):
+def exercicio2(P: np.matrix, X_0: int, n: int, qtd: int):
     validar_matriz(P, 4)
-    raise NotImplementedError
+    ans = 0
+    for k in range(qtd):
+        ans += (simulaca_markov(P,X_0, n))**2
+    return ans/qtd
