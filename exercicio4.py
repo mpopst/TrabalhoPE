0d6dbed4525f2e805edd74c2f36ac7a6a94ed8de
mpopst <ma.popst@hotmail.com>
2019-11-11 02:08:12 -0300

    Mudanças no exercicio 4

--- exercicio4.py
+++ exercicio4.py
@@ -3,14 +3,26 @@ import numpy as np
 def entre_chegadas(lamb: int, T: int):
     ans = [0]
     while ans[-1] < T:
-        ans.append(ans[-1] + np.random.exponential(lamb))
-    ans.append(T)
+        step = np.random.exponential(lamb)
+        if ans[-1] + step > T:
+            break
+        else:
+            ans.append(ans[-1] + step)
     return ans
 
 def teorema448(lamb: int, T: int):
-    n = lamb*T
-    ans = [0, 5]
+    n = int(lamb*T)
+    ans = [0]
     for i in range(n):
         ans.append(np.random.uniform(low=0, high=T))
     ans.sort()
     return ans
+
+def estima_integral(temposMudanca, T):
+    NT = len(temposMudanca)
+    multiplicando = NT * (T - temposMudanca[-1])
+    somatorio = 0
+    for k in range(1,NT-1):
+        somatorio += k * (temposMudanca[k+1] - temposMudanca[k])
+    return somatorio * multiplicando
+
