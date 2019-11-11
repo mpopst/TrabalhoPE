888b716927749dbbe742968650846919b383d39f
Vitória Guardieiro <viigua@gmail.com>
2019-11-10 23:56:55 -0300

    atualiza exercicio4 e relatorio

--- exercicio4.py
+++ exercicio4.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def entre_chegadas(lamb: int, T: int) -> np.array:
+def entre_chegadas(lamb: int, T: int):
     ans = [0]
     while ans[-1] < T:
         ans.append(ans[-1] + np.random.exponential(lamb))
     ans.append(T)
-    return np.array(ans)   
+    return ans
 
-def teorema448(lamb: int, T: int) -> np.array:
+def teorema448(lamb: int, T: int):
     n = lamb*T
     ans = [0, 5]
     for i in range(n):
